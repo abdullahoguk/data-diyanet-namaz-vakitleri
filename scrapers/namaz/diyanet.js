f4c8faaae5185b7fffb5afc1b3ef004851ce4f9b
Abdullah <abdullahoguk@gmail.com>
2022-01-02 19:08:11 +0300

    Update diyanet.js

--- scrapers/namaz/diyanet.js
+++ scrapers/namaz/diyanet.js
@@ -17,7 +17,7 @@ async function start() {
         var plate = il;
         var diyanetId = CITIES[il].diyanetId;
         var name = CITIES[il].name;
-        var url = DIYANET_BASE_URL + diyanetId + "/" + HAVA_URLS[il].slug + "-icin-namaz-vakti"
+        var url = DIYANET_BASE_URL + diyanetId + "/" + CITIES[il].slug + "-icin-namaz-vakti"
         //console.log("Fetching... " + name);
         //entities.decodeXML("")
         var fullRawHTML = await fetchAsync(url);
